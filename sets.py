4364bcd4b5d229243e89383bb3604293d8a74d9a
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-05-24 11:36:43 -0400

    Update sets.py

--- sets.py
+++ sets.py
@@ -16,6 +16,6 @@ def check_drinks(drink_name, drink_ingredients):
     for i in drink_ingredients:
         new_set.add(i)
     if len(new_set.intersection(ALCOHOLS))>0:
-        return drink_name.append('Cocktail')
+        return str(drink_name)+'Cocktail'
     else:
-        return drink_name.append('Mocktail')
+        return str(drink_name)+'Mocktail'
