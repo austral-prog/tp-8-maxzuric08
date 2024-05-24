7b04a3b0eb6c843c99d5e21a76f9414af1017eb3
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-05-24 11:33:05 -0400

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,7 +2,7 @@ from sets_categories_data import (ALCOHOLS)
 
 
 def clean_ingredients(dish_name, dish_ingredients):
-    new_set={}
+    new_set=set()
     for i in dish_ingredients:
         if i not in new_set:
             new_set.add(i)
@@ -12,7 +12,7 @@ def clean_ingredients(dish_name, dish_ingredients):
 
 
 def check_drinks(drink_name, drink_ingredients):
-    new_set={}
+    new_set=set()
     for i in drink_ingredients:
         new_set.add(i)
     if len(new_set.intersection(ALCOHOLS)>0):
