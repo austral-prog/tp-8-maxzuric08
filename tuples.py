c4c15837688d78507b940c35772a5c57da207b51
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-05-24 10:28:22 -0400

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,35 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
-
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+    return record[1]
+
+
+"""Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
 
     :param record: tuple - with a (treasure, coordinate) pair.
     :return: str - the extracted map coordinate.
-    """
+"""
 
-    return ""
 
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
+    new_format=()
+    for i in coordinate:
+        new_format+=i
+    return new_format
+
+"""Split the given coordinate into tuple containing its individual components.
 
     :param coordinate: str - a string map coordinate
     :return: tuple - the string coordinate split into its individual components.
-    """
+"""
 
-    return ()
+
 
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
+    join_coordinate=""
+    for i in rui_record[1]:
+        join_coordinate+=i
+    if azara_record.count(join_coordinate)==1:
+        return azara_record+rui_record
+    else:
+        return "not a match"
