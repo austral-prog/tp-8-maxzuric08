6dddf7ee93c23e3e313395ae93b4b8df4e847fe9
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-05-24 11:26:31 -0400

    Update sets.py

--- sets.py
+++ sets.py
@@ -12,16 +12,8 @@ def clean_ingredients(dish_name, dish_ingredients):
 
 
 def check_drinks(drink_name, drink_ingredients):
-    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
-
-    :param drink_name: str - name of the drink.
-    :param drink_ingredients: list - ingredients in the drink.
-    :return: str - drink_name appended with "Mocktail" or "Cocktail".
-
-    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
-    name followed by "Cocktail" (includes alcohol).
-
-    """
-
-    return ""
+    if len(drink_ingredients(ALCOHOLS)>0):
+        return drink_name.append('Cocktail')
+    else:
+        return drink_name.append('Mocktail')
 
